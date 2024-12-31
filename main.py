f18b3e13af07f24ee65a62364e34aab313c08fe0
Dev-Graham <grahamcbowen@gmail.com>
2024-12-30 21:15:36 -0500

    Initial commit

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))
 
-    pygae.display.flip()
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
